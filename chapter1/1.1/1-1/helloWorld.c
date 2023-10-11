5b18706ef56cd3c3baf296fdc37cff7a667b39b4
marlon-buckley <marlon.buckley@protonmail.com>
2023-10-11 10:34:01 +0100

    first few excercises

---
+++ chapter1/1.1/1-1/helloWorld.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("hello, world\n");
+}
+
+// a C program whatever its size, consists of functions and variables
+// "main" is special, its the point at which a program begins executing
+// every program must have a main somewhere
+
+// printf() never supplies a newline on its own, yu can use several calls to build up a single output line
+
+// printf ("hello, "); 
+// printf ("world" ); 
+// printf("\n"); }
+
+// escape sequences; can use \t for tab, \b for backspace, \" for double quote and \\ for a backslash itself
---
+++ chapter1/1.2/1-3/farenheitCelsiusTable.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    float fahr, celsius;
+    float lower, upper, step;
+
+    lower = 0;      /* lower limit of tempreture table */
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size*/ 
+
+    fahr = lower;
+    printf("Farenheit   Celsius\n");
+
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
